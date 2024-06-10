cafd67e927ba4e9b1ecc1d3cbbff7d9eb2951c4c
Igor Dubois <igorduboispro@gmail.com>
2024-06-10 12:27:18 +0200

    need to solve the last exercise on array cop

--- 05_arrayOperations.js
+++ 05_arrayOperations.js
@@ -10,7 +10,9 @@
  * 
   */
 
-const multiplyByTwo = (array) => {}
+const multiplyByTwo = (array) => array.map(a => a*2);
+
+console.log(multiplyByTwo([1,2]));
 
 
 /**
@@ -22,9 +24,9 @@ const multiplyByTwo = (array) => {}
  *   - les mots clées function et return sont interdits
  *   - Vous ne pouvez pas utiliser de variable (autre que l'argument de la fonction)
   */
-
-const filterNameStartByA = (array) => {}
-
+const vocab = ["Armure", "Bizarre"]
+const filterNameStartByA = (array) => array.filter((word) => word[0] ==="A");
+console.log(filterNameStartByA(vocab));
 /**
  * Utiliser la fonction .reduce sur le tableau passé en paramètre
  * retourne la somme des valeurs du tableau
@@ -35,7 +37,8 @@ const filterNameStartByA = (array) => {}
  *   - Vous ne pouvez pas utiliser de variable (autre que l'argument de la fonction)
   */
 
-const sum = (array) => {}
+const sum = (array) => (array.reduce((acc,curr) => acc + curr, 0));
+console.log(sum([1,2,3]));
 
 /**
  * Utiliser la fonction .find sur le tableau passé en paramètre
@@ -56,7 +59,7 @@ const sum = (array) => {}
  *   - Vous ne pouvez pas utiliser de variable (autre que l'argument de la fonction)
   */
 
-const findUserById = (array, id) => {}
+const findUserById = (array, id) => array.find(user.id === id)
 
 
 module.exports = {multiplyByTwo, filterNameStartByA, sum, findUserById};
